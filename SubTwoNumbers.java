4ccdd3626eb2a4694d6cc9edc5250759dcfc956b
Atharva Dahiwal <139111195+Atharva-Dahiwal@users.noreply.github.com>
2023-07-14 21:14:19 +0530

    Add files via upload

---
+++ SubTwoNumbers.java
@@ -0,0 +1,9 @@
+public class SubTwoNumbers {
+    public static void main(String[] args) {
+    int num1 = 10;
+    int num2 = 5;
+    int sub;
+    sub = num1 - num2;
+    System.out.println("The subtraction of two numbers: "+sub);
+}
+}
