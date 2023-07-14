885a686319408bff87b0aaa0a65bb6bb87702678
Atharva Dahiwal <139111195+Atharva-Dahiwal@users.noreply.github.com>
2023-07-14 21:43:00 +0530

    Add files via upload

---
+++ MultiTwoNumbers.java
@@ -0,0 +1,9 @@
+public class MultiTwoNumbers {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+        int multi;
+        multi = num1 * num2;
+        System.out.println("The multiplication of two numbers: "+multi);
+    }
+}
