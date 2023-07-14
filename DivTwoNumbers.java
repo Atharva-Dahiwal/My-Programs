61a729accfb0d98470dc81b05537b3c4e2c3076d
Atharva Dahiwal <139111195+Atharva-Dahiwal@users.noreply.github.com>
2023-07-14 21:49:11 +0530

    Add files via upload

---
+++ DivTwoNumbers.java
@@ -0,0 +1,9 @@
+public class DivTwoNumbers {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+        int div;
+        div = num1 / num2;
+        System.out.println("The division of two numbers: "+div);
+    }
+}
