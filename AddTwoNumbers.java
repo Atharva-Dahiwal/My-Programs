0df5f1267184992d0cf20dcfa64035df355a7805
Atharva Dahiwal <139111195+Atharva-Dahiwal@users.noreply.github.com>
2023-07-14 13:49:42 +0530

    Add files via upload

---
+++ AddTwoNumbers.java
@@ -0,0 +1,11 @@
+class AddTwoNumbers {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int sum;
+    sum = a + b;
+        System.out.print("The sum of two numbers: "+sum);
+
+    }
+    
+}
