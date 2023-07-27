07fb292c74ab938bec1cdf023524fd5aba1ece8a
Atharva Dahiwal <139111195+Atharva-Dahiwal@users.noreply.github.com>
2023-07-27 22:00:24 +0530

    CelciustoFahrenheit.java

---
+++ CelciustoFahrenheit.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class test {
+  public static void main(String[] args) {
+    float temperature = sc.nextInt();
+    System.out.println("Enter Temperature: "+temperature);
+    float fahrenheit;
+    fahrenheit = (temperature*9/5) + 32;
+    System.out.println("Fahrenheit is: "+fahrenheit);
