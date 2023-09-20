3a275ad01e11952bd6c1e2dee739f5160907816a
Atharva Dahiwal <139111195+Atharva-Dahiwal@users.noreply.github.com>
2023-09-20 18:45:42 +0530

    PositiveorNegative.java
    In this program I have not used the else statement

---
+++ PositiveorNegative.java
@@ -0,0 +1,15 @@
+// This program is created using only if statements 
+import java.util.*;
+public class practice {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a number");
+    int num = sc.nextInt();
+   if(num>0){
+    System.out.println(num+" is Positive");
+   } if(num<=0){
+    System.out.println(num+" is Negative");
+   }
+
+    }
+}
