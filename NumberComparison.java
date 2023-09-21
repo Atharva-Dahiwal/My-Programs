97842824ba916eca8855f8482320b15fa6937993
Atharva Dahiwal <139111195+Atharva-Dahiwal@users.noreply.github.com>
2023-09-21 20:37:26 +0530

    NumberComparison.java
    In this program I have not used the else statement

---
+++ NumberComparison.java
@@ -0,0 +1,15 @@
+// In this program I have not used the else statement
+import java.util.*;
+public class practice {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+System.out.println("Enter two numbers");
+int a = sc.nextInt();
+int b = sc.nextInt();
+if(a>b) {
+    System.out.println("A is greater");
+} if(b>a) {
+    System.out.println("B is greater");
+}
+    }
+}
