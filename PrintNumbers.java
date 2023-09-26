dcaeef24c573c70ffeb767a84068b006d2f5ca83
Atharva Dahiwal <139111195+Atharva-Dahiwal@users.noreply.github.com>
2023-09-26 19:58:05 +0530

    PrintNumbers.java
    This program prints numbers from 1 to n using for loop

---
+++ PrintNumbers.java
@@ -0,0 +1,12 @@
+// This program prints number from 1 to n 
+import java.util.*;
+public class practice {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+System.out.println("Enter a number ");
+int num = sc.nextInt();
+for(int i=1;i<=num;i++) {
+  System.out.println(i);
+}
+    }
+  }
